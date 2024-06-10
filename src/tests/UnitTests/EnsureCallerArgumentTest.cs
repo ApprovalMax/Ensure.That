04086dbdfd1420c4f8486e64d76473c33e07c812
Alexander Dyatlov <alexander.dyatlov@approvalmax.com>
2024-06-10 10:55:47 +0100

    Add EnsureCallerArgumentTest

---
+++ src/tests/UnitTests/EnsureCallerArgumentTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnsureThat;
+using FluentAssertions;
+using Xunit;
+
+namespace UnitTests
+{
+    public class EnsureCallerArgumentTest
+    {
+        [Fact]
+        public void ArgumentNullException_Correctly_Captures_Null_Objects_Name_For_EnsureArgIsNotNull()
+        {
+            var nullObject = default(object);
+            Action act = () => EnsureArg.IsNotNull(nullObject);
+            act.Should().Throw<ArgumentNullException>().WithParameterName(nameof(nullObject));
+        }
+    }
+}
