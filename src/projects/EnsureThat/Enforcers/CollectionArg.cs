4d1d2264a6adb36dde83606f3fbaa85f113a8f84
Vsevolod <seva1398@mail.ru>
2024-07-08 13:26:34 +0400

    fix;

--- src/projects/EnsureThat/Enforcers/CollectionArg.cs
+++ src/projects/EnsureThat/Enforcers/CollectionArg.cs
@@ -411,8 +411,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public ICollection<T> HasAllNotNullItems<T>([ValidatedNotNull, NotNull]ICollection<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : class
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => item is null))
@@ -428,8 +426,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public IReadOnlyCollection<T> HasAllNotNullItems<T>([ValidatedNotNull, NotNull]IReadOnlyCollection<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : class
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => item is null))
@@ -445,8 +441,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public IReadOnlyList<T> HasAllNotNullItems<T>([ValidatedNotNull, NotNull]IReadOnlyList<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : class
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => item is null))
@@ -462,8 +456,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public ISet<T> HasAllNotNullItems<T>([ValidatedNotNull, NotNull]ISet<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : class
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => item is null))
@@ -479,8 +471,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public T[] HasAllNotNullItems<T>([ValidatedNotNull, NotNull]T[] value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : class
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => item is null))
@@ -496,8 +486,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public IList<T> HasAllNotNullItems<T>([ValidatedNotNull, NotNull] IList<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : class
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => item is null))
@@ -513,8 +501,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public Collection<T> HasAllNotNullItems<T>([ValidatedNotNull, NotNull] Collection<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : class
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => item is null))
@@ -530,8 +516,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public List<T> HasAllNotNullItems<T>([ValidatedNotNull, NotNull] List<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : class
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => item is null))
@@ -547,8 +531,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public HashSet<T> HasAllNotNullItems<T>([ValidatedNotNull, NotNull] HashSet<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : class
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => item is null))
@@ -564,8 +546,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public ICollection<T> HasAllNotDefaultItems<T>([ValidatedNotNull, NotNull]ICollection<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : struct
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => default(T).Equals(item)))
@@ -581,8 +561,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public IReadOnlyCollection<T> HasAllNotDefaultItems<T>([ValidatedNotNull, NotNull]IReadOnlyCollection<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : struct
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => default(T).Equals(item)))
@@ -599,8 +577,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public IReadOnlyList<T> HasAllNotDefaultItems<T>([ValidatedNotNull, NotNull]IReadOnlyList<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : struct
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => default(T).Equals(item)))
@@ -616,8 +592,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public ISet<T> HasAllNotDefaultItems<T>([ValidatedNotNull, NotNull]ISet<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : struct
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => default(T).Equals(item)))
@@ -633,8 +607,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public T[] HasAllNotDefaultItems<T>([ValidatedNotNull, NotNull]T[] value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : struct
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => default(T).Equals(item)))
@@ -650,8 +622,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public IList<T> HasAllNotDefaultItems<T>([ValidatedNotNull, NotNull] IList<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : struct
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => default(T).Equals(item)))
@@ -667,8 +637,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public Collection<T> HasAllNotDefaultItems<T>([ValidatedNotNull, NotNull] Collection<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : struct
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => default(T).Equals(item)))
@@ -684,8 +652,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public List<T> HasAllNotDefaultItems<T>([ValidatedNotNull, NotNull] List<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : struct
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => default(T).Equals(item)))
@@ -701,8 +667,6 @@ namespace EnsureThat.Enforcers
         [ContractAnnotation("value:null => halt")]
         public HashSet<T> HasAllNotDefaultItems<T>([ValidatedNotNull, NotNull] HashSet<T> value, [InvokerParameterName, CallerArgumentExpression(nameof(value))] string paramName = null, OptsFn optsFn = null) where T : struct
         {
-            Ensure.Any.IsNotNull(value, paramName);
-
             Ensure.Collection.HasItems(value, paramName);
 
             if (value.Any(item => default(T).Equals(item)))
